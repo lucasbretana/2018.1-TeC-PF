5c8ca5b49a549239fc5f74473defde3bc17d0a2c
Lucas E. Bretana <lucasbretana@gmail.com>
2018-06-05 12:54:13 -0300

    Last line is not justified anymore, also added some collegues examples for tests

--- Task1/task1.hs
+++ Task1/task1.hs
@@ -5,7 +5,7 @@
 --justifica :: String -> String
 justifica [] = []
 justifica s = let list = toList s 
-              in concat $ map (flip(justLine) (longestString list)) list
+              in (concat ( (map (flip(justLine) (longestString list)) (init list)) ) ) ++ ( last list )
 --justifica _ = error "To implement"
 
 {--
@@ -65,3 +65,33 @@ longestString (x:xs)
   where
     o = longestString xs
 
+
+-- Example test
+texto = "RUBIÃO fitava a enseada -- eram oito horas da manhã.\n\
+  \Quem o visse com os polegares metidos no cordão do chambre à janela de uma\n\
+  \grande casa de Botafogo cuidaria que ele admirava aquele pedaço de água\n\
+  \quieta mas em verdade vos digo que pensava em outra coisa.\n\
+  \Cotejava o passado com o presente. Que era há um ano?\n\
+  \Professor. Que é agora? Capitalista! Olha para si para as chinelas\n\
+  \(umas chinelas de Túnis que lhe deu recente amigo Cristiano Palha) para a casa\n\
+  \para o jardim para a enseada para os morros e para o\
+  \céu e tudo desde as chinelas\n\
+  \até o céu tudo entra na\
+  \mesma sensação de propriedade."
+
+
+-- Examples from Jonathas
+
+textoEx2 = "Jonathas Augusto\nde Oliveira\nConceição"
+
+textoEx3 =
+  "\"Lorem ipsum\" text is derived from sections 1.10.33 of Cicero's De finibus bonorum et malorum.[1]\n\
+  \\n\
+  \It is not known exactly when the text obtained its current standard form; it may have been as late as the 1960s. Dr.\n\
+  \Richard McClintock, a Latin scholar who was the\npublications director at Hampden–Sydney College in Virginia, discovered\n\
+  \the source of the passage sometime before 1982 while searching for instances of the Latin\n\
+  \word \"consectetur\" (\"that [he/she/it] pursue\", subjunctive), rarely used in classical literature.[2][a]\n\
+  \The physical source of the lorem ipsum text may be the 1914 Loeb\nClassical Library Edition of the De Finibus,\n\
+  \where the Latin text, presented on the left-hand (even) pages, breaks off on page 34 with \"Neque porro quisquam\n\
+  \est qui do-\" and continues on page 36 with \"lorem ipsum ...\", suggesting that the galley type of that page was\n\
+  \mixed up to make the dummy text seen today.[4]"
