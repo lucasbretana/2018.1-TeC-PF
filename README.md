824a3d7fdb28da154521f3ec24da18fee2df8e77
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2018-06-04 22:46:09 -0300

    Not done yet

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TeC-PF
+# TeC-PF - sites.google.com/site/haskellufpel
 Haskell classes
+
+# Parser
+  - Receive a expression in a string, and
+  - Wil return the same expression in a data format
+  
+  __Notes__
+  - There can be only + and * operations, 
+  - The precende is left to right (as usual)
+  - The expression may contain parenthesis to change precendence of operations
+
+  __Ideia for solution__
+  - DONE
+    - _tokenizer_: a function that given a string it return the first token on that, and the rest of the string
+
+  - TODO
+    - a function that receive that look for:
+      - '+' then the interprets the left part and A and right part as B
+      - '*' then the interprets the left part and A and right part as B
+      - '(' then interprets everything inside as an expression
+      - Num Int: witch will be a final expression
--- tokenizer.hs
+++ tokenizer.hs
@@ -1,64 +1,27 @@
+import Data.List.Index
+
 data E  = Num Int
         | Sum E E
         | Mul E E
   deriving (Show, Eq)
 
-data Ele = Single E | List [E]
-
-data Token = Plus | Times | OPar | CPar | Crap Int | EOF
+data Token = Plus | Times | OPar | CPar | N Int | EOF
   deriving(Show, Eq)
 
 parse :: String -> E
-parse [] = error "Empty string"
+parse []          = error "Empty string"
+--parse str@(x:xs)  = 
 
+divideOp :: [(Int, Token)] -> 
 
-{--
- - Takes the current expressio  list, the string of the remaining expression and the current token
- - Returns a list of expression using sublists for sub expressions
---}
-expAsList :: [Ele] -> [String] -> Token -> [Ele]
--- end of expression
-expAsList cur [] EOF   = cur
-expAsList _   _  EOF   = error "Makes no sense to receive a EOF, but the expression string is not over"
-expAsList _   [] Plus  = error "Makes no sense a '+' in the end"
-expAsList _   [] Times = error "Makes no sense a '*' in the end"
-expAsList _   [] OPar  = error "Makes no sense a '(' in the end"
---expAsList cur [] a    = cur++[a]
--- sub expression
-expAsList cur strExp OPar = takeSubExp ['('] 1 str
-  where
-     {--
-     - Simply takes a string, and return any inside parentesis substring
-     --}
-     takeSubExp :: String -> Int -> String -> String
-     takeSubExp subStr 0 _   = subStr
-     takeSubExp _      _ []  = error "End of expression string is incorrect. Abort"
-     takeSubExp subStr n str@('(':xs) = takeSubExp (subStr++['(']) (n+1) xs
-     takeSubExp subStr n str@(')':xs) = takeSubExp (subStr++[')']) (n-1) xs
-     takeSubExp subStr n str@(x:xs)   = takeSubExp (subStr++[x]) n xs
+expAsList :: String -> [Token]
+expAsList [] = EOF:[]
+expAsList strExp@('+':xs) = Plus:(expAsList xs)
+expAsList strExp@('*':xs) = Times:(expAsList xs)
+expAsList strExp@('(':xs) = OPar:(expAsList xs)
+expAsList strExp@(')':xs) = CPar:(expAsList xs)
+expAsList str             = num:(expAsList rem)
+                                  where (num, rem) = readNum str
 
-
---           (input -> (token, remain)
-tokenizer :: String -> (Token, String)
-tokenizer []  = (EOF, [])
-tokenizer str@(x:xs)
-  | x == '('  = (OPar, xs)
-  | x == '+'  = (Plus, xs)
-  | x == ')'  = (CPar, xs)
-  | x == '*'  = (Times, xs)
-  | otherwise = let (t, s) = readNum str
-                  in (Crap t, s)
-
-readNum :: String -> (String, String)
-readNum str = (takeWhile (\a -> elem a ['0'..'9']) str, dropWhile (\a -> elem a ['0'..'9']) str)
-
-{--
-findMul :: String -> String -> String -> E
-findMul str@(x:xs) a b
-  | x == '*'  = Mul (parse a) (parse (tail b))
-  | otherwise = findMul x:a b (tail b)
---}
-
-
-exp :: String
-exp = "0 + (2 + 20) * 2" -- Sum 0 (Mul (Sum 2 20) 2)
+readNum :: String -> (Token, String)
+readNum sExp = ( N (read ( takeWhile (\a -> elem a ['0'..'9']) sExp )::Int), dropWhile (\b -> elem b ['0'..'9']) sExp )
